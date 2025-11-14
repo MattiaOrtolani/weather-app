d9ed8791930ff65384b0d0ee3b2b18dbb014a8af
5kul1y <mattia.ortolani05@gmail.com>
2025-11-14 18:34:06 +0100

    feat: improved loading item

--- src/app/loading-item/loading-item.component.ts
+++ src/app/loading-item/loading-item.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class LoadingItemComponent {
 
+  loading_text = ["P", "r", "e", "p", "a", "r", "o", "i", "l", "m", "e", "t", "e", "o", ".", ".", "."];
 }
