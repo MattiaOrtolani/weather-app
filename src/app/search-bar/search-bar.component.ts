61a4be546db4b35dfd46fd57024243c1e2043cab
5kul1y <mattia.ortolani05@gmail.com>
2025-12-02 17:38:33 +0100

    feat: handle geolocation and city search errors

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,25 +1,24 @@
-import { Component, Input, output } from '@angular/core';
+import { Component, input, Input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-search-bar',
-  standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+    selector: 'app-search-bar',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './search-bar.component.html',
+    styleUrl: './search-bar.component.scss',
 })
 export class SearchBarComponent {
-  isOpen = false;
-  @Input() input: string = '';
-  readonly search = output<string>();
+    isOpen = false;
+    @Input() input: string = '';
+    readonly search = output<string>();
+    errorMessage = input<string>('');
 
-  OnSearchClick()
-  {
-    if(!this.isOpen && this.input.length > 0)
-    {
-      this.search.emit(this.input);
-      this.input = '';
+    OnSearchClick() {
+        if (!this.isOpen && this.input.length > 0) {
+            this.search.emit(this.input);
+            this.input = '';
+        }
     }
-  }
 }
